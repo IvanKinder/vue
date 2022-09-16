7fd60337a1b9393241ac2f7cde292418eb7a1de9
Ivan Kinder <kinder_iv@magnit.ru>
2022-09-17 00:28:58 +0300

    lesson 3

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,56 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <MyCalc/>
+    <header>
+      <div class="header">
+        My Personal costs
+      </div>
+    </header>
+    <main>
+      <AddPaymentForm @add-payment="addPayment"/>
+      <PaymentsDisplay :paymentsList="paymentsList"/>
+    </main>
   </div>
 </template>
 
 <script>
-import MyCalc from './components/MyCalc.vue'
+import PaymentsDisplay from './components/PaymentsDisplay.vue'
+import AddPaymentForm from './components/AddPaymentForm.vue'
 
 export default {
   name: 'App',
   components: {
-    MyCalc
+    PaymentsDisplay,
+    AddPaymentForm
+  },
+  data: () => ({
+    paymentsList: []
+  }),
+  methods: {
+    fetchPaymentsData() {
+      return [
+        {
+          date: '28.03.2020',
+          category: 'Food',
+          value: 169
+        },
+        {
+          date: '24.03.2020',
+          category: 'Transport',
+          value: 360
+        },
+        {
+          date: '28.03.2020',
+          category: 'Food',
+          value: 532
+        }
+      ]
+    },
+    addPayment(data) {
+      this.paymentsList.push(data)
+    }
+  },
+  created() {
+    this.paymentsList = this.fetchPaymentsData()
   }
 }
 </script>
